aa9dce19248874a3cc455ebb9e71d493457245ef
CCherry07 <2405693142@qq.com>
2022-10-13 16:12:46 +0800

    feat:  list-items update

--- src/components/status-buttons.exercise.js
+++ src/components/status-buttons.exercise.js
@@ -10,9 +10,7 @@ import {
   FaTimesCircle,
 } from 'react-icons/fa'
 import Tooltip from '@reach/tooltip'
-// 🐨 you'll need useQuery, useMutation, and queryCache from 'react-query'
 import { useQuery, useMutation, queryCache } from 'react-query';
-// 🐨 you'll also need client from 'utils/api-client'
 import { client } from 'utils/api-client';
 import { useAsync } from 'utils/hooks'
 import * as colors from 'styles/colors'
@@ -50,16 +48,10 @@ function TooltipButton({ label, highlight, onClick, icon, ...rest }) {
 }
 
 function StatusButtons({ user, book }) {
-  // 🐨 call useQuery here to get the listItem (if it exists)
-  // queryKey should be 'list-items'
-  // queryFn should call the list-items endpoint
   const { data: listItems } = useQuery({
     queryKey: 'list-items',
     queryFn: () => client("list-items", { token: user.token }).then(data => data.listItems)
   })
-  console.log(listItems);
-  // 🐨 search through the listItems you got from react-query and find the
-  // one with the right bookId.
   const listItem = listItems?.find(li => li.bookId === book.id) || null
   const [create] = useMutation(({ bookId }) => client("list-items", { data: { bookId }, token: user.token }),
     {
@@ -69,21 +61,11 @@ function StatusButtons({ user, book }) {
     {
       onSettled: () => queryCache.invalidateQueries("list-items")
     })
-  // 💰 for all the mutations below, if you want to get the list-items cache
-  // updated after this query finishes then use the `onSettled` config option
-  // to queryCache.invalidateQueries('list-items')
+  const [update] = useMutation((updates) => client(`list-items/${updates.id}`, { method: "PUT", data: updates, token: user.token }),
+    {
+      onSettled: () => queryCache.invalidateQueries("list-items")
+    })
 
-  // 🐨 call useMutation here and assign the mutate function to "update"
-  // the mutate function should call the list-items/:listItemId endpoint with a PUT
-  //   and the updates as data. The mutate function will be called with the updates
-  //   you can pass as data.
-
-  // 🐨 call useMutation here and assign the mutate function to "remove"
-  // the mutate function should call the list-items/:listItemId endpoint with a DELETE
-
-  // 🐨 call useMutation here and assign the mutate function to "create"
-  // the mutate function should call the list-items endpoint with a POST
-  // and the bookId the listItem is being created for.
 
   return (
     <React.Fragment>
@@ -92,18 +74,14 @@ function StatusButtons({ user, book }) {
           <TooltipButton
             label="Unmark as read"
             highlight={colors.yellow}
-            // 🐨 add an onClick here that calls update with the data we want to update
-            // 💰 to mark a list item as unread, set the finishDate to null
-            // {id: listItem.id, finishDate: null}
+            onClick={() => update({ id: listItem.id, finishDate: null })}
             icon={<FaBook />}
           />
         ) : (
           <TooltipButton
             label="Mark as read"
             highlight={colors.green}
-            // 🐨 add an onClick here that calls update with the data we want to update
-            // 💰 to mark a list item as read, set the finishDate
-            // {id: listItem.id, finishDate: Date.now()}
+            onClick={() => update({ id: listItem.id, finishDate: Date.now() })}
             icon={<FaCheckCircle />}
           />
         )
