2540e1e5bd834b18334e8de3f56570a99ccfd873
CCherry07 <2405693142@qq.com>
2022-10-14 20:45:36 +0800

    feat:  queryCache clear

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,10 +1,11 @@
 // 🐨 get the queryCache from 'react-query'
 import * as auth from 'auth-provider'
+import { queryCache } from "react-query"
 const apiURL = process.env.REACT_APP_API_URL
 
 async function client(
   endpoint,
-  {data, token, headers: customHeaders, ...customConfig} = {},
+  { data, token, headers: customHeaders, ...customConfig } = {},
 ) {
   const config = {
     method: data ? 'POST' : 'GET',
@@ -19,11 +20,10 @@ async function client(
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
     if (response.status === 401) {
-      // 🐨 call queryCache.clear() to clear all user data from react-query
       await auth.logout()
-      // refresh the page for them
+      queryCache.clear()
       window.location.assign(window.location)
-      return Promise.reject({message: 'Please re-authenticate.'})
+      return Promise.reject({ message: 'Please re-authenticate.' })
     }
     const data = await response.json()
     if (response.ok) {
@@ -34,4 +34,4 @@ async function client(
   })
 }
 
-export {client}
+export { client }
