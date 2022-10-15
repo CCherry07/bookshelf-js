aaa5701adf4a3322580d7c43ca947741358b3e3d
CCherry07 <2405693142@qq.com>
2022-10-15 09:15:19 +0800

    feat:  status button state with hooks

--- src/components/status-buttons.exercise.js
+++ src/components/status-buttons.exercise.js
@@ -10,12 +10,10 @@ import {
   FaTimesCircle,
 } from 'react-icons/fa'
 import Tooltip from '@reach/tooltip'
-import { useQuery } from 'react-query';
-import { client } from 'utils/api-client';
 import { useAsync } from 'utils/hooks'
 import * as colors from 'styles/colors'
 import { CircleButton, Spinner } from './lib'
-import { useUpdateListItem, useRemoveListItem, useCreateListItem } from '../utils/list-items';
+import { useUpdateListItem, useRemoveListItem, useCreateListItem, useListItem } from '../utils/list-items';
 function TooltipButton({ label, highlight, onClick, icon, ...rest }) {
   const { isLoading, isError, error, run } = useAsync()
 
@@ -48,11 +46,7 @@ function TooltipButton({ label, highlight, onClick, icon, ...rest }) {
 }
 
 function StatusButtons({ user, book }) {
-  const { data: listItems } = useQuery({
-    queryKey: 'list-items',
-    queryFn: () => client("list-items", { token: user.token }).then(data => data.listItems)
-  })
-  const listItem = listItems?.find(li => li.bookId === book.id) || null
+  const listItem = useListItem(user, book.id)
   const [create] = useCreateListItem(user)
   const [remove] = useRemoveListItem(user)
   const [update] = useUpdateListItem(user)
